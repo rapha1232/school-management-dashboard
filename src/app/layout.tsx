0d4e278dba12cf04160e90c9d57161b9f072ac2b
Raphael-Semaan <rapha.z1232@icloud.com>
2024-09-23 19:37:38 +0300

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children} <ToastContainer position="bottom-right" theme="dark" />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        colorSky: "#C3EBFA",
+        colorSkyLight: "#EDF9FD",
+        colorPurple: "#CFCEFF",
+        colorPurpleLight: "#F1F0FF",
+        colorYellow: "#FAE27C",
+        colorYellowLight: "#FEFCE8",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
